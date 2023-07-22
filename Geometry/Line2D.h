cef6da4329c28d7a83e0dae2f8b7f3104a3da85e
BorisVandermeer <463201013@qq.com>
2023-07-22 13:52:02 +0800

    Add Circle2D

--- Geometry/Line2D.h
+++ Geometry/Line2D.h
@@ -8,18 +8,20 @@
 
 #pragma once
 
-#include "Vectors/Vector2D.h"
+#include <Vectors/Vector2D.h>
 
 namespace Geometry {
 
-  class  Line {
-  public:
+    class  Line {
+    public:
+      
+        Line(){};
+        Vectors::Vector2D direction;
+        Vectors::Vector2D point;
+    };
 
-      Line(){};
-      Vectors::Vector2D direction;
-      Vectors::Vector2D point;
-  };
+    Points::PosPoint2D GetIntersection(Line const &  l1, Line const &  l2);
 
-  Points::PosPoint2D GetIntersection(Line l1, Line l2);
+    bool isParallel(Line const & l1, Line const & l2);
 
 } // namespace Geometry 
